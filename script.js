0777ac8ff6c8ef25bc850f368018cb60f096a1b4
Sagulucky <slakshmisagar825@gmail.com>
2025-08-30 23:28:54 +0530

    Completed project: Simple Calculator

--- script.js
+++ script.js
@@ -7,17 +7,21 @@ buttons.forEach(button => {
   button.addEventListener("click", () => {
     const value = button.textContent;
 
-    if (button.classList.contains("number") || button.classList.contains("operator") || button.classList.contains("decimal")) {
+    if (button.classList.contains("number") || button.classList.contains("operator") || button.classList.contains("decimal") || button.classList.contains("zero") ) {
       currentInput += value;
       result.value = currentInput;
     } 
     else if (button.classList.contains("clear")) {
       currentInput = "";
       result.value = "";
+    }
+    else if (button.classList.contains("del")) {
+      currentInput = currentInput.slice(0, -1);
+      result.value = currentInput;
     } 
     else if (button.classList.contains("equals")) {
       try {
-        currentInput = eval(currentInput); // ⚠️ Simple, but risky if used in real projects
+        currentInput = eval(currentInput);
         result.value = currentInput;
       } catch {
         result.value = "Error";
